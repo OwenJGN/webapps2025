8d6101bdcf22e9ecc91858873176e9dfd06c6474
Owen <owenjgibson@gmail.com>
2025-03-10 16:10:53 +0000

    Updated urls

--- register/urls.py
+++ register/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='register/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(template_name='register/logout.html'), name='logout'),
+    # Update logout view to accept GET requests
+    path('logout/', auth_views.LogoutView.as_view(template_name='register/logout.html', next_page='home'), name='logout'),
     path('profile/', views.profile, name='profile'),
 ]
