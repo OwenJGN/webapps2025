7c5c42cd9edd985955d0675fa4c7e439fbfb9e4f
Owen <owenjgibson@gmail.com>
2025-04-01 19:57:22 +0100

    Updated Admin Functions

--- webapps2025/urls.py
+++ webapps2025/urls.py
@@ -6,7 +6,6 @@ from django.urls import path, include
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     # Home page without any redirects
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
     # Application URLs
